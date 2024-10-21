68c7bb944b685df093b586c5ad16518467c02a86
Danila Goriachkin <dndv279@gmail.com>
2024-10-21 21:17:14 +0300

    Create project

---
+++ SimpleIosCi/SimpleIosCi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleIosCi
+//
+//  Created by Danila on 21.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleIosCi/SimpleIosCiTests/SimpleIosCiTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleIosCiTests.swift
+//  SimpleIosCiTests
+//
+//  Created by Danila on 21.10.2024.
+//
+
+import XCTest
+@testable import SimpleIosCi
+
+final class SimpleIosCiTests: XCTestCase {
+    
+    func testExample() throws {
+        XCTAssertEqual(1, 1)
+    }
+}
